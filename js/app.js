196f761a5f9c49c4d948d6182391eb99ea387f4f
nikos theodoropoulos <ntheodoropoulos@outlook.com>
2020-12-10 18:07:46 +0200

    mvps done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
-})
+
+  const coolForm = document.querySelector('#new-item-form');
+  coolForm.addEventListener('submit', handleFormSubmit);
+
+  // console.log('JavaScript loaded');
+});
+
+
+// function
+const handleFormSubmit = function(event) {
+  event.preventDefault();
+
+  // grabbing the unordered list
+  const list = document.querySelector('#reading-list');
+
+  // grab the user input
+  const title = event.target.title.value;
+  const author = event.target.author.value;
+  const category = event.target.category.value;
+  console.log(title, author, category);
+  
+  // create a list item element 
+  const newListItemTitle = document.createElement('li');
+  newListItemTitle.textContent = title;
+
+  const newListItemAuthor = document.createElement('li');
+  newListItemAuthor.textContent = author;
+
+  const newListItemCategory = document.createElement('li');
+  newListItemCategory.textContent = category;
+
+  // create a horizontal line item
+  const horizontalLine = document.createElement('hr');
+
+  // append the newly created item into the list?
+  list.appendChild(newListItemTitle);
+  list.appendChild(newListItemAuthor);
+  list.appendChild(newListItemCategory);
+  list.appendChild(horizontalLine);
+  // document.getElementById("new-item-form").reset();
+  document.querySelector('#new-item-form').reset();
+};
